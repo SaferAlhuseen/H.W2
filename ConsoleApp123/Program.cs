e4430af5af40e51ec9ab6622812c4a877574b965
FREEMAN <FREEMAN@DESKTOP-RODT8RQ>
2024-12-12 10:50:37 +0300

    Add project files.

---
+++ ConsoleApp123/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication59
+{
+    public class Vehicle
+    {
+        public string Name;
+        class MotorVehicle : Vehicle
+        {
+            public string modelalName;
+        }
+        class Car : MotorVehicle
+        { }//فارغ
+        class truck : MotorVehicle
+        { }//فارغ
+        class Program
+        {//رئيسي
+            static void Main(string[] args)
+            {
+                Car MyCar = new Car();
+                MyCar.Name = "MERCEDES";
+                MyCar.modelalName = "1996";
+                truck MyTruck = new truck();
+                MyTruck.Name = "BMW";
+                MyTruck.modelalName = "1998";
+                Console.WriteLine("My Car name and model is: " +
+                MyCar.Name + " and " + MyCar.modelalName);
+                Console.WriteLine("My Truck name and model is : " +
+                MyTruck.Name + " and " + MyTruck.modelalName);
+                Console.ReadKey();
+            }
+        }
+    }
+}
